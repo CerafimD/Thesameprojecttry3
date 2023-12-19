f8a53f56da442583684662423fa2b58af69869b0
Danil Serafim <danilcerafim@gmail.com>
2023-12-19 14:49:11 +0500

    task2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.models.Vacancy;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Vacancy> vacancies = org.example.CSV.readVacancies("src/main/resources/vacancies.csv");
+        System.out.println(vacancies);
     }
 }
